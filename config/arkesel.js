1f0edc1c701dc5c7039410daede4124b27cd1a96
Adzam Komla <46463371+adzamkomladev@users.noreply.github.com>
2021-04-04 23:51:31 +0000

    Update and rename arkesel.js to kasa.js

--- config/arkesel.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    sender_id: process.env.ARKESEL_SMS_SENDER_ID || "Hello",
-    api_key: process.env.ARKESEL_SMS_API_KEY || "khg28y2hna*nqbhw*",
-    
-}
---
+++ config/kasa.js
@@ -0,0 +1,5 @@
+module.exports = {
+    sender_id: process.env.KASA_SMS_SENDER_ID || "KASA",
+    api_key: process.env.KASA_SMS_API_KEY || "khg28y2hna*nqbhw*",
+    
+}
